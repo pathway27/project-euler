868c835238023926d743bdb629135e940ba7cffb
pathway27 <pathway27@gmail.com>
2014-07-29 23:26:16 +1000

    .

--- python/p017.py
+++ python/p017.py
@@ -9,6 +9,7 @@ how many letters would be used?
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) 
 contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. 
 The use of "and" when writing out numbers is in compliance with British usage.
+
 """
 
 words = [
